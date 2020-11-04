7583245430b67698d450da960c1840effa6800d0
aspil <aspil@github.com>
2020-11-04 21:59:05 +0200

    Forgot to add struct map_node and struct map

--- Project 1/include/map.h
+++ Project 1/include/map.h
@@ -1,8 +1,25 @@
 #pragma once
 typedef unsigned int (*HashFunction) (void*);
-typedef unsigned int (*CompareFunction) (void*);
-typedef unsigned int (*DeleteKeyFunction) (void*);
-typedef unsigned int (*DeleteValueFunction) (void*);
+typedef int (*CompareFunction) (void*, void*);
+typedef void (*DeleteKeyFunction) (void*);
+typedef void (*DeleteValueFunction) (void*);
+
+struct map_node {
+	void *key;
+	void *value;
+	struct map_node *next;
+};
+
+struct hash_map {
+	struct map_node **array;
+	struct map_node **last_chain_bucket;
+	int size;
+	int count;
+	HashFunction hash;
+	CompareFunction compare;;
+	DeleteKeyFunction delete_key;
+	DeleteValueFunction delete_value;
+};
 
 struct hash_map* map_init(
 	unsigned int size,
