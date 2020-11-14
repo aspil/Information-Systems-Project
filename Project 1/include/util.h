304d0a6f05028995bafa0be40fc7dc19da9df18a
aspil <aspil@github.com>
2020-11-14 15:34:42 +0200

    Moved the dataset parsing functions from util.h to dataset_parsing.h

--- Project 1/include/util.h
+++ Project 1/include/util.h
@@ -13,9 +13,7 @@
 void strip_ext(char *filename);
 int count_files(char*path);
 int pick_the_buckets(int arg_c,char **arg_v);
-int read_data_X(struct hash_map *ptr,int size,char *path);
 void hash_the_node(struct clique *ptr,char *path,char *id, char *site);
-void dataset_y(struct hash_map *map,char *path);
 void get_line_without_end_line(char *str);
 void print_results(struct hash_map *map);
 int compare_array(Array dyn_arr,struct clique *address);
