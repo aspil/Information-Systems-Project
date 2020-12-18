577a95b0c7cfa51149a135b414ea8eae9c52c5a5
fanmfh <73366336+FanouriosStylianosPsathopoulos@users.noreply.github.com>
2020-12-18 02:11:06 +0200

    Delete types.h

--- Project 2/types.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-typedef unsigned int (*HashFunction) (void*);
-typedef int (*CompareFunction) (void*, void*);
-typedef void (*DeleteKeyFunction) (void*);
-typedef void (*DeleteValueFunction) (void*);
