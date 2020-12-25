cc3ea9b78bbf9c00f179d3a1e6839e7e422c2d66
aspil <aspil@github.com>
2020-12-26 00:36:33 +0200

    Minor stuff

--- Project 2/src/util.c
+++ Project 2/src/util.c
@@ -123,166 +123,11 @@ int pick_the_buckets(int argc,char **argv)
 }
 
 
-// void construct_product(struct clique **ptr,char *path,char *id, char *site)
-// {
-
-// 	struct vector *vec;
-// 	FILE *fp;
-// 	char *line = NULL,*temp_1 = NULL,*temp2 = NULL,*temp_3 = NULL;
-// 	size_t len = 0;
-// 	size_t read;
-// 	char *str;
-// 	fp = fopen (path,"r");
-// 	if (fp  ==  NULL)
-// 		printf ("Cannot open directory '%s'\n", path);
-
-// 	strip_ext(id);	// Remove '.json' from the filename
-// 	// Initialazation of the product
-// 	struct product *p = product_init(atoi(id), site, (*ptr));
-	
-// 	(*ptr)->first_product = p;
-
-// 	(*ptr)->last_product = p;
-// 	(*ptr)->size += 1; // Increase the size by 1
-
-// 	/* Begin reading the json file */
-// 	while ((read = getline(&line, &len, fp)) != -1) 
-// 	{
-// 		// take the line of the file
-// 		str = line;
-// 		if (line[0] != '{' && line[0] != '}') // ignore { and }
-// 		{
-// 			int i, x; // code snippet to ignore whitespaces
-// 			for(i = x = 0; str[i]; ++i)
-// 				if(!isspace(str[i]) || (i > 0 && !isspace(str[i-1])))
-// 				 	str[x++] = str[i];
-// 			str[x] = '\0';
-
-// 			// format sequence 
-// 			if (str[0] == '"')
-// 			{
-// 				str = str+1;
-// 				if (str[0] == '<')
-// 					str = str+1; // important for the first spec : page title 
-			   
-// 				// we got rid of the first symbols
-
-// 				char *helping_str = str;
-
-// 				while (helping_str[0] != '"') 
-// 				{
-// 					if (helping_str[0] == '>') // code snipet for page title end
-// 						break;
-					
-// 					else if (helping_str[0] == '\\') // code snipet for " that dont declare the end in this case 
-// 						if (helping_str[1] == '"')
-// 							helping_str++; 
-
-// 					helping_str++;
-// 				}
-// 				helping_str[0] = '\0';
-				
-// 				temp_1 = malloc(strlen(str) + 1); // the spec characteristic, free'd in util.c
-// 				strcpy(temp_1,str);
-
-// 				/* Find its value by passing these characters <whitespaces,:> */
-// 				str = strlen(str) + 2 + str;
-// 				while (str[0] != '"' && str[0] != '[') 
-// 					str = str+1;
-				
-// 				if (str[0] == '"')
-// 				{
-// 					str = str+1;  // pass the "
-					
-// 					helping_str = str;
-
-// 					while (helping_str[0] != '"')
-// 					{
-// 						if (helping_str[0] == '\\')
-// 							if (helping_str[1] == '"') // surpass this "
-// 								helping_str++;
-
-// 						helping_str++;
-// 					}
-// 					helping_str[0] = '\0';
-// 					temp2 = malloc(strlen(str) + 1);	// Gets free'd by vector_delete
-// 					strcpy(temp2,str);
-// 					// we have both values spec and value in temp 
-// 					// we can pass it
-// 					vec = vector_init(1, free);
-// 					vector_push_back(vec, temp2);
-// 					push_specs((*ptr), temp_1, vec);
-// 					vector_delete(vec);	
-// 				}
-// 				else if (str[0] == '[') /* There is a list of values that we need to store */
-// 				{
-// 					int counter = 0;
-// 					str = str+1;
-
-// 					/* the values  will be one on each next line */
-
-// 					while ((read = getline(&line, &len, fp)) != -1)
-// 					{
-// 						str = line;
-// 						for(i = x = 0; str[i]; ++i)
-// 							if(!isspace(str[i]) || (i > 0 && !isspace(str[i-1]))) // delete the whitespaces 
-// 								str[x++] = str[i];
-
-// 						str[x] = '\0';
-						
-// 						if (str[0] == '"')
-// 						{
-// 							str = str+1;
-
-// 							helping_str = str;
-// 							while (helping_str[0] != '"')
-// 							{
-// 								if (helping_str[0] == '\\')
-// 									if (helping_str[1] == '"') // surpass this "
-// 										helping_str++;
-
-// 								helping_str++;
-// 							}
-// 							helping_str[0] = '\0';
-// 							counter++;
-
-// 							if (counter == 1)
-// 							{
-// 								// Initialazation of the array
-// 								temp_3 = malloc(strlen(str) + 1);	// Gets free'd by vector_delete
-// 								strcpy(temp_3,str);
-// 								vec = vector_init(1, free);
-// 								vector_push_back(vec, temp_3);
-// 							}
-// 							else
-// 							{
-// 								temp_3 = malloc(strlen(str) + 1);	// Gets free'd by vector_delete
-// 								strcpy(temp_3,str);
-// 								vector_push_back(vec, temp_3);
-// 							}		
-// 						}
-// 						else 
-// 						{
-// 							push_specs((*ptr), temp_1, vec);
-// 							break;
-// 						}
-// 					}
-// 					vector_delete(vec);
-// 				}
-// 				free(temp_1);
-// 			}
-// 		} 
-// 	}
-// 	free(line);
-// 	fclose (fp);
-// }
-
-
 int print_results(struct hash_map *map) {
 	int counter=0,relations=0;
-	struct vector *vec;
+	struct vector *vec = NULL;
 	struct map_node *ptr;
-	struct clique *last,*print_ptr;
+	struct clique **last;
 	struct product *iteration_first_product;
 	struct product *print_product;
 	int result=0;
@@ -291,36 +136,35 @@ int print_results(struct hash_map *map) {
 	{
 		ptr = map->array[i];
 
-		while (ptr!= NULL) //there are things to see
+		while (ptr != NULL) //there are things to see
 		{
-			if (counter==0)
+			if (counter == 0)
 			{
 				vec = vector_init(1, NULL);
-				vector_push_back(vec,map->array[i]->value);
+				vector_push_back(vec, *(struct clique**) map->array[i]->value);
 				counter++;
-				last=ptr->value;
-				iteration_first_product=last->first_product;
-				while (iteration_first_product!=NULL)
+				last = (struct clique **)ptr->value;
+				iteration_first_product = (*last)->first_product;
+				while (iteration_first_product != NULL)
 				{
-					print_product=iteration_first_product->next;
-					while (print_product!=NULL)
+					print_product = iteration_first_product->next;
+					while (print_product != NULL)
 					{
 						relations++;
-						printf("%s//%d %s//%d \n",iteration_first_product->website,iteration_first_product->id,print_product->website,print_product->id );
-						print_product=print_product->next;
+						printf("%s//%d %s//%d \n", iteration_first_product->website, iteration_first_product->id,print_product->website,print_product->id );
+						print_product = print_product->next;
 					}
-					iteration_first_product=iteration_first_product->next;
+					iteration_first_product = iteration_first_product->next;
 				}
 			}
 			else
-			{
-				result = vector_search_clique(vec, ptr->value); //checks if the clique you want to print has already been printed 
-			}
-			if (result==-1) 
+				result = vector_search_clique(vec, *(struct clique**) ptr->value); //checks if the clique you want to print has already been printed 
+
+			if (result == -1) 
 			{
 				// you can print this clique
-				last=ptr->value;
-				iteration_first_product = last->first_product;
+				last = (struct clique **)ptr->value;
+				iteration_first_product = (*last)->first_product;
 				while (iteration_first_product != NULL)
 				{
 					print_product = iteration_first_product->next;
@@ -332,9 +176,9 @@ int print_results(struct hash_map *map) {
 					}
 					iteration_first_product = iteration_first_product->next;
 				}
-				vector_push_back(vec,last);  
+				vector_push_back(vec, *(struct clique**) last);
 			}
-		   ptr=ptr->next;      //check the next bucket
+		   ptr = ptr->next;      //check the next bucket
 		}	
 	} 
 	vector_delete(vec);  
@@ -343,7 +187,7 @@ int print_results(struct hash_map *map) {
 }
 
 int print_negative_results(struct hash_map *map) {
-	int counter=0,relations=0;
+	int counter = 0, relations = 0;
 	struct vector *vec = NULL;
 	struct map_node *ptr;
 	struct clique **last;
@@ -362,30 +206,29 @@ int print_negative_results(struct hash_map *map) {
 
 				vector_push_back(vec, *(struct clique**) map->array[i]->value);
 				counter++;
-				//printf("The clique consists of:\n");
 				last = (struct clique **)ptr->value;
-				iteration_first_product=(*last)->first_product;
+				iteration_first_product = (*last)->first_product;
 
 				printf("The clique consists of:\n");
 				
 				while (iteration_first_product!=NULL)
 				{
 					printf("%s//%d \n",iteration_first_product->website, iteration_first_product->id);
-					iteration_first_product=iteration_first_product->next;
+					iteration_first_product = iteration_first_product->next;
 				}
 
 				printf("The negative has the following members: \n");
 
-				struct negative_relation *list_ptr=(*last)->first_negative;
+				struct negative_relation *list_ptr = (*last)->first_negative;
 
-				while (list_ptr!=NULL)
+				while (list_ptr != NULL)
 				{
-					iteration_first_product=list_ptr->neg_rel->first_product;
+					iteration_first_product = list_ptr->neg_rel->first_product;
 
-					while (iteration_first_product!=NULL)
+					while (iteration_first_product != NULL)
 					{
 						printf("%s//%d \n",iteration_first_product->website, iteration_first_product->id);
-						iteration_first_product=iteration_first_product->next;
+						iteration_first_product = iteration_first_product->next;
 					}
 
 					list_ptr=list_ptr->next;
@@ -393,19 +236,18 @@ int print_negative_results(struct hash_map *map) {
 
 			}
 			else
-			{
 				result = vector_search_clique(vec, *(struct clique**) ptr->value); //checks if the clique you want to print has already been printed 
-			}
+
 			if (result == -1) 
 			{
 				// you can print this clique
 				last = (struct clique **)ptr->value;
 				
-				iteration_first_product=(*last)->first_product;
+				iteration_first_product = (*last)->first_product;
 
 				printf("The clique consists of:\n");
 				
-				while (iteration_first_product!=NULL)
+				while (iteration_first_product != NULL)
 				{
 					printf("%s//%d \n",iteration_first_product->website, iteration_first_product->id);
 					iteration_first_product=iteration_first_product->next;
@@ -413,24 +255,24 @@ int print_negative_results(struct hash_map *map) {
 
 				printf("The negative has the following members: \n");
 
-				struct negative_relation *list_ptr=(*last)->first_negative;
+				struct negative_relation *list_ptr = (*last)->first_negative;
 
-				while (list_ptr!=NULL)
+				while (list_ptr != NULL)
 				{
-					iteration_first_product=list_ptr->neg_rel->first_product;
+					iteration_first_product = list_ptr->neg_rel->first_product;
 
-					while (iteration_first_product!=NULL)
+					while (iteration_first_product != NULL)
 					{
-						printf("%s//%d \n",iteration_first_product->website, iteration_first_product->id);
-						iteration_first_product=iteration_first_product->next;
+						printf("%s//%d \n", iteration_first_product->website, iteration_first_product->id);
+						iteration_first_product = iteration_first_product->next;
 					}
 					printf("end\n");
-					list_ptr=list_ptr->next;
+					list_ptr = list_ptr->next;
 				}
 				
 				vector_push_back(vec,*(struct clique**) last);  
 			}
-		   ptr=ptr->next;      //check the next bucket
+		   ptr = ptr->next;      //check the next bucket
 		}	
 	} 
 	vector_delete(vec);  
