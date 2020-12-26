365eaf34c9354249025636f4c719a5f9f66a9050
FanouriosStylianosPsathopoulos <sdi1400320@di.uoa.gr>
2020-12-26 15:57:11 +0200

    Testing update

--- Project 2/src/main.c
+++ Project 2/src/main.c
@@ -47,6 +47,10 @@ int get_stopwords(char *stopwords_file) {
 }
 
 int main(int argc, char *argv[]) {
+	srand(time(NULL));
+
+	row = 0;
+	column = 0;
 	int size;
 	if ((size = pick_the_buckets(argc, argv)) <= 0)
 		return -1;
@@ -54,22 +58,78 @@ int main(int argc, char *argv[]) {
 	/* Safe assignments, error checking was done in pick_the_buckets */
 	char *data_path = argv[1];	
 	char *relations_file = argv[2];
-	printf("Hash table size: %d \n",size );
-
-	/* Initialize the hash table */
-	struct hash_map *map = map_init(size, hash_str, NULL, free, delete_clique); 
-	/* Parse the json files */
-	read_data_files(map,size,data_path);
-
-	/* Parse the relations and change the cliques accordingly */
-	read_relations(map,relations_file);
+	learning_rate = 0.01;
+	int max_features = atoi(argv[3]);
+	// train_test_split(model,relations_file,0.6,0.4);
 	
-	/* Print the results of the cliques */
-	int l = print_results(map);
-	printf("%d \n",l);
+	/* Initialize the hash table */
+	struct hash_map *map = map_init(size, hash_str, compare_str, free, delete_clique);
 
+	// /* Construct the stopwords dictionary */
+	stopwords = map_init(37, hash_str, compare_str, free, NULL);
+	get_stopwords("Datasets/stopwords.txt");
+
+	// printf("New hash table size: %d \n",map->size);
+	// /* Parse the json files */
+	// printf("Constructing clique relationships...\n");
+	read_data_files(map, size, data_path);
+
+	printf("Generating the word vectors...\n");
+
+	//struct vectorizer *tfidf = vectorizer_init(size, 1);	// 1 means tfidf instead of bow
+	//vectorizer_fit_transform(tfidf, data_path, max_features);
+
+	printf("Initializing the logistic regression model\n");
+	//struct LogisticRegressor *model = Logistic_Regression_Init();
+
+	printf("Split train and test datasets\n");
+	//train_test_split(model, relations_file, 0.6, 0.4);
+
+	// printf("Fitting the data into our model\n");
+	//Logistic_Regression_fit(model, tfidf);
+
+	// printf("Training...\n");
+	// map_print(tfidf->features);
+	//train_test_split(model, relations_file, 0.6, 0.4);
+	//train(model, "Datasets/train_labels.csv");
+
+	//vectorizer_delete(tfidf);
+
+	// /* Parse the relations and change the cliques accordingly */
+	read_relations(map,relations_file);
+	/* Print the results of the cliques */
+
+	struct all_info *start=NULL;
+
+	start=malloc(sizeof(struct all_info ));
+	start->size=0;
+	start->first=NULL;
+
+	read_relations_2(map,relations_file,start);
+	int l = print_results(map);
+	printf("THe integer is %d \n",l);
+
+	struct list_negative *tranverse=start->first;
+
+	//int ok=print_negative_results(map);
+
+	//printf("%d\n", ok);
+
+	make_the_files(map);
+
+	int sum=0;
+
+	while (tranverse!=NULL)
+	{
+		printf("%d %d \n",tranverse->ptr_1->size,tranverse->ptr_2->size );
+		sum=sum + (tranverse->ptr_1->size * tranverse->ptr_2->size);
+		tranverse=tranverse->next;
+	}
+
+	printf("Sum is %d \n",sum );
 	/* Free the allocated memory of the hash table */
-	map_delete(map);
+	// map_delete(map);
+	// map_delete(stopwords);
 	
 	return 0;
 }
