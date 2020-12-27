418a7d4bc56817f94de118693f6c3b452cd5fda7
aspil <sdi1700147@di.uoa.gr>
2020-12-27 14:25:23 +0200

    Minor change

--- Project 2/include/types.h
+++ Project 2/include/types.h
@@ -19,27 +19,11 @@ struct pair {
 		char b_c;
 		char *b_s;
 	};
+	// void *a_i;
+	// void *b_i;
 };
 struct triple {
 	int a_i;
 	double b_d;
 	double c_d;
-	// union {
-	// 	int a_i;
-	// 	double a_d;
-	// 	char a_c;
-	// 	char *a_s;
-	// };
-	// union {
-	// 	int b_i;
-	// 	double b_d;
-	// 	char b_c;
-	// 	char *b_s;
-	// };
-	// union {
-	// 	int c_i;
-	// 	double c_d;
-	// 	char c_c;
-	// 	char *c_s;
-	// };
 };
